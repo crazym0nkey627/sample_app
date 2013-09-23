fbe91e5d7416279f11a217c47360d5e84200b6e3
Franklin <franklin.wong0@gmail.com>
2013-09-23 13:59:26 -0700

    Add a full_title_helper

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,21 @@
 class StaticPagesController < ApplicationController
   def home
+	puts "YOU CALLED THE HOME FUNCTION"
+	1 + 1
   end
 
   def help
+	puts "YOU CALLED THE HELP FUNCTION"
+	2 + 2
   end
   
   def about
+	puts "YOU CALLED THE ABOUT FUNCTION"
+	3 + 3
+  end
+  
+  def contact
+	puts "YOU CALLED THE CONTACT FUNCTION"
+	4 + 4
   end
 end
